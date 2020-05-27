f8e6acde6b010ee0ef31cf3cbcf3743515f4c3c6
LeviSchouten <levischoutenschool@gmail.com>
2020-05-27 22:01:28 +0200

    started on upload form

--- client/src/views/Upload.vue
+++ client/src/views/Upload.vue
@@ -1,10 +1,44 @@
 <template lang="pug">
   .upload
     h1 UPLOAD
-    form(action="/api/upload" method="POST")
-      input#file(type="file" name="file")
-      br
-      input#fileTitle(name="title")
-      br
-      button#submit upload!
+    el-form(:model="form" ref="form" label-width="120px")
+      el-upload(drag action="https://jsonplaceholder.typicode.com/posts/")
+        i.el-icon-upload
+        .el-upload__text Drop file here 
+          em click to upload
+        .el-upload__tip(slot="tip") Video files smaller then 1gb?
+      el-form-item(label="title")
+        el-input(v-model="form.title")
+      el-form-item(label="description")
+        el-input(type="textarea" v-model="form.description")
+    //- form(action="/api/upload" method="POST")
+      //- input#file(type="file" name="file")
+      //- br
+      //- input#fileTitle(name="title")
+      //- br
+      //- button#submit upload!
+    
 </template>
+
+<script>
+export default {
+  name: "Upload",
+  data() {
+    return {
+      form: {
+        title: "",
+        description: "",
+        tags: [],
+        video: {}
+      }
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.upload {
+  max-width: 400px;
+  margin: 0 auto;
+}
+</style>
