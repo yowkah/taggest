60f105193603a0dc28841fdfcfdbcf721eabe60c
Jouke Ypma <joukeypma@gmail.com>
2020-06-12 23:18:41 +0200

    added basic tags functionality

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,10 +5,24 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    tags: [],
   },
   mutations: {
+    ADD_TAG({ tags }, tag) {
+      tags.push(tag);
+    },
   },
   actions: {
+    getTags({ commit }) {
+      fetch('/api/video/tags')
+        .then((data) => data.json())
+        .then((tags) => commit('ADD_TAG', tags));
+    },
+    addTag({ commit, state }, tag) {
+      if (state.tags.indexOf(tag) === -1) {
+        commit('ADD_TAG', tag);
+      }
+    },
   },
   modules: {
   },
