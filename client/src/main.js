22732d493f52a802df441d9da466da426d826835
Jouke Ypma <joukeypma@gmail.com>
2020-06-12 23:18:13 +0200

    fixed linting issues

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
@@ -11,5 +11,5 @@ Vue.use(ElementUI);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
