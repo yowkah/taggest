cfb05ce11e62e02c50faea7b797e5570a90a9528
LeviSchouten <levischoutenschool@gmail.com>
2020-06-15 01:19:23 +0200

    added vuex mutations

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -6,24 +6,32 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     tags: [],
+    whitelist: [],
+    blacklist: [],
   },
   mutations: {
     ADD_TAG({ tags }, tag) {
       tags.push(tag);
+    },
+    ADD_TAGS_IF_MISSING({ tags }, tagList) {
+      const missingTags = tagList.filter((tag) => !tags.includes(tag));
+      missingTags.forEach((tag) => tags.push(tag));
+    },
+    SET_WHITELIST(state, tags) {
+      Vue.set(state, 'whitelist', tags);
+    },
+    SET_BLACKLIST(state, tags) {
+      Vue.set(state, 'blacklist', tags);
     },
   },
   actions: {
     getTags({ commit }) {
       fetch('/api/video/tags')
         .then((data) => data.json())
-        .then((tags) => commit('ADD_TAG', tags));
-    },
-    addTag({ commit, state }, tag) {
-      if (state.tags.indexOf(tag) === -1) {
-        commit('ADD_TAG', tag);
-      }
+        .then((tags) => {
+          tags.forEach((tag) => commit('ADD_TAG', tag));
+        });
     },
   },
-  modules: {
-  },
+  modules: {},
 });
