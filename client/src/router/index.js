33265a073ad11fa3480543039b567abc811491c3
Jouke Ypma <joukeypma@gmail.com>
2020-07-22 01:10:19 +0200

    linting
    Co-authored-by: LeviSchouten <levischoutenschool@gmail.com>

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -16,12 +16,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Upload.vue'),
+    component() {
+      return import(/* webpackChunkName: "about" */ '../views/Upload.vue');
+    },
   },
 ];
 
-const router = new VueRouter({
+export default new VueRouter({
   routes,
 });
-
-export default router;
